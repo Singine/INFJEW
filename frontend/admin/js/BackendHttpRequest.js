d398a1c1aa4cdf73229fcbdcc2cfa6da46cbdbc1
Edric.D <singine@live.cn>
2025-05-26 20:49:08 +0800

    50
    50

--- frontend/admin/js/BackendHttpRequest.js
+++ frontend/admin/js/BackendHttpRequest.js
@@ -1,6 +1,7 @@
-document
-  .getElementById("login-submit-btn")
-  .addEventListener("click", function () {
+// 登录按钮事件监听
+const loginSubmitBtn = document.getElementById("login-submit-btn");
+if (loginSubmitBtn) {
+  loginSubmitBtn.addEventListener("click", function () {
     const username = document.getElementById("login-username").value.trim();
     const password = document.getElementById("login-password").value.trim();
 
@@ -18,6 +19,7 @@ document
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log("Response data:", data); // 打印响应数据以便调试
         if (data.success) {
           console.log("Login successful");
           window.location.href = "http://dashboard.infjew.com";
@@ -26,21 +28,26 @@ document
         }
       });
   });
+}
 
-document.getElementById("logout-btn").addEventListener("click", function () {
-  fetch("http://www.infjew.com/api/AuthLogout", {
-    method: "POST", // 使用 POST 方法
-    headers: {
-      "Content-Type": "application/json", // 设置请求头，指定内容格式为 JSON
-    },
-  })
-    .then((response) => response.json()) // 解析 JSON 响应
-    .then((data) => {
-      if (data.success) {
-        console.log("Logout successful");
-        // 这里可以清除前端的用户状态，例如删除存储的 token 或清空用户信息
-      } else {
-        console.log("Error:", data.message);
-      }
-    });
-});
+// 登出按钮事件监听
+const logoutBtn = document.getElementById("logout-btn");
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", function () {
+    fetch("http://www.infjew.com/api/AuthLogout", {
+      method: "POST", // 使用 POST 方法
+      headers: {
+        "Content-Type": "application/json", // 设置请求头，指定内容格式为 JSON
+      },
+    })
+      .then((response) => response.json()) // 解析 JSON 响应
+      .then((data) => {
+        if (data.success) {
+          console.log("Logout successful");
+          // 这里可以清除前端的用户状态，例如删除存储的 token 或清空用户信息
+        } else {
+          console.log("Error:", data.message);
+        }
+      });
+  });
+}
