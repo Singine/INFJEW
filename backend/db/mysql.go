403eb55a51565e325b1fd3b697c5e906c8d1c7ad
Edric.D <singine@live.cn>
2025-05-26 16:30:29 +0800

    18
    18

--- backend/db/mysql.go
+++ backend/db/mysql.go
@@ -1,21 +1,21 @@
-package db
+// package db
 
-import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
-)
+// import (
+// 	"database/sql"
+// 	_ "github.com/go-sql-driver/mysql"
+// 	"log"
+// )
 
-var DB *sql.DB
+// var DB *sql.DB
 
-func InitDB() {
-	var err error
-	dsn := "root:Haishigugue33@tcp(127.0.0.1:3306)/infjew"
-	DB, err = sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal("数据库连接失败:", err)
-	}
-	if err = DB.Ping(); err != nil {
-		log.Fatal("数据库无法访问:", err)
-	}
-}
+// func InitDB() {
+// 	var err error
+// 	dsn := "root:Haishigugue33@tcp(127.0.0.1:3306)/infjew"
+// 	DB, err = sql.Open("mysql", dsn)
+// 	if err != nil {
+// 		log.Fatal("数据库连接失败:", err)
+// 	}
+// 	if err = DB.Ping(); err != nil {
+// 		log.Fatal("数据库无法访问:", err)
+// 	}
+// }
