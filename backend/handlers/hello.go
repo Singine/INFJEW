fee3096b474fc59683e72888cf5ae263afb33e9a
Edric.D <singine@live.cn>
2025-05-26 12:17:16 +0800

    11
    11

--- backend/handlers/hello.go
+++ backend/handlers/hello.go
@@ -6,6 +6,11 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
+
+	// 添加 CORS 允许
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Content-Type", "text/plain")
+
 	if r.Method != http.MethodGet {
 		http.Error(w, "只允许 GET 请求", http.StatusMethodNotAllowed)
 		return
