a62b2f5ac9654f193c8a8d8aacf848c9d7579db6
Edric.D <singine@live.cn>
2025-05-26 11:16:23 +0800

    3
    3

--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"backend/handlers" // 替换成你的 module 名
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.HandleFunc("/check-id", handler.CheckIDHandler)
+
 	port := ":8080"
 	fmt.Printf("服务器正在运行在 %s\n", port)
-	
+
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("无法启动服务器:", err)
